820d69c4f14bcdffc524056d30b5debab1b2f30f
girish sharma <girishk.sharma@dotsqaures.com>
2017-06-06 16:00:41 +0530

    comment added

--- GitUseExample/ViewController.m
+++ GitUseExample/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // just a comment to test remote git commit
 }
 
 
