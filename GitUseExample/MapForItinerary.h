4d9554e07220b4f7d363a65fe49be1a126119e02
Amit <girishk.sharma@dotsquares.com>
2017-06-06 12:31:41 +0530

    Started work on new map feature, not quite done yet.

---
+++ GitUseExample/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitUseExample
+//
+//  Created by Amit on 6/6/17.
+//  Copyright © 2017 Dotsquares. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
---
+++ GitUseExample/MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.m
+//  GitUseExample
+//
+//  Created by Amit on 6/6/17.
+//  Copyright © 2017 Dotsquares. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+@implementation MapForItinerary
+
+@end
