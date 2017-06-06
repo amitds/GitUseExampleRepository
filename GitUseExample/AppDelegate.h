c1ab9377cd1cbf7fa34cf25d5282f399f1862880
Amit <girishk.sharma@dotsquares.com>
2017-06-06 11:39:44 +0530

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Amit on 6/6/17.
+//  Copyright © 2017 Dotsquares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Amit on 6/6/17.
+//  Copyright © 2017 Dotsquares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
